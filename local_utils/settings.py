28c05d11a95a393ac0ecf7c0cb7dcb270ba9cdb3
Sully Sullenberger <sully@sadburger.com>
2023-09-17 08:52:20 -0700

    add clarifai requirement

--- local_utils/settings.py
+++ local_utils/settings.py
@@ -6,8 +6,9 @@ from pydantic import BaseModel, Field, SecretStr, field_validator
 
 class StreamlitAppSettings(BaseModel):
     clarifai_pat: str = Field(default_factory=lambda: st.secrets["CLARIFAI_PAT"])
-    app_data: Path = Field(default_factory=lambda: Path(st.secrets["APP_DATA"]))
     openai_api_key: SecretStr = Field(default_factory=lambda: st.secrets["OPENAI_API_KEY"], validate_default=True)
+
+    app_data: Path = Field(default_factory=lambda: Path(st.secrets["APP_DATA"]))
     session_data: Path = Field(default_factory=lambda: Path(st.secrets["SESSION_DIR"]))
 
     dynamodb_thoughts_table: str = Field(default_factory=lambda: st.secrets["DYNAMODB_THOUGHTS_TABLE"])
